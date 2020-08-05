b949e4f97e1aaa84750986d7cdc8766bb555fe5c
whigon <1054733646@qq.com>
2020-08-06 02:54:22 +0800

    Optimize

--- app/src/main/java/com/example/yl/Dao/BaseDao.java
+++ app/src/main/java/com/example/yl/Dao/BaseDao.java
@@ -5,40 +5,36 @@ import com.j256.ormlite.dao.Dao;
 import java.sql.SQLException;
 import java.util.List;
 
-public abstract class BaseDao {
+public abstract class BaseDao<T> {
     public abstract <T> Dao<T, Integer> getDao();
 
-    public <T> void insert(T t) {
+    public void insert(T t) {
         try {
-            Dao<T, Integer> dao = getDao();
-            dao.create(t);
+            getDao().create(t);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public <T> void update(T t) {
+    public void update(T t) {
         try {
-            Dao<T, Integer> dao = getDao();
-            dao.update(t);
+            getDao().update(t);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public <T> void delete(T t) {
+    public void delete(T t) {
         try {
-            Dao<T, Integer> dao = getDao();
-            dao.delete(t);
+            getDao().delete(t);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public <T> List<T> getAll() {
+    public List<T> getAll() {
         try {
-            Dao<T, Integer> dao = getDao();
-            return dao.queryForAll();
+            return (List<T>) getDao().queryForAll();
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
